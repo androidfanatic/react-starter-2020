c1518dc424db9cf0a669b371ec7073b1dd83caad
Manish <manish_raj@outlook.com>
2020-01-31 17:42:05 +0530

    feat: add info addon

--- src/widgets/button/Button.stories.tsx
+++ src/widgets/button/Button.stories.tsx
@@ -1,10 +1,12 @@
 import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
 import React from 'react';
 import Button from './Button';
 
 export default {
   title: 'Button',
   component: Button,
+  decorators: [withInfo],
 };
 
 export const Text = () => <Button label={'Get Reviews'} onClick={action('clicked')} />;
